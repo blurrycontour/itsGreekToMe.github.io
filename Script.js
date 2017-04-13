b6a1b76418d332a4ca76fbed8a09e94c8fae0948
Aditya Singh <adityasingh8532@gmail.com>
2017-04-13 06:50:53 +0530

    Delete Script.js

--- Script.js
+++
@@ -1,9 +0,0 @@
-document.getElementById("foot01").innerHTML =
-"<p>&copy;  " + new Date().getFullYear() + "  --  Author ---  Aditya Singh</p>";
-
-document.getElementById("nav01").innerHTML =
-"<ul id='menu'>" +
-"<li><a href='index.html'>Home</a></li>" +
-"<li><a href='About.html'>About</a></li>" +
-"<li><a href='Information.html'>Information</a></li>"+
-"</ul>";
